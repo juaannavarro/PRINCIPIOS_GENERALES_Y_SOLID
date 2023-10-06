7035a948955fb51c878912266c9b2557be40b99e
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2023-10-06 19:02:18 +0200

    matriz

--- ejercicio2.py
+++
@@ -1,5 +0,0 @@
-"crud, crear, actualizar, modificar y eliminar"
-"get, post, put, delete"
-
-"Authenticator: manejar la autenticación de usuarios(nombre, apellido, telefono, mail)"
-"LOGIN, REGISTER"
